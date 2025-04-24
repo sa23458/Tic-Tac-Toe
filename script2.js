e2c416ca8569063e83edb22e2f8324324b0a5c5d
MUHAMMAD SHAWAIZ <shawaiz@dcaptech.org>
2025-04-24 14:02:22 +0500

    first comit

---
+++ script2.js
@@ -0,0 +1,15 @@
+let students = ["arbaz","Faisal","Zakir", "muhammad bilal"];
+let countries = [
+    ["Pakistan", "India", "Afghanistan"],
+    ["China", "United States of donald trump", "UK"],
+    ["Canada", "chile", "Mexico"],
+]
+//for(let i = 0; i<students.length; i++){
+  //  console.log(students[i]);
+//}
+
+// for of loop 
+for(let country of countries){
+    console.log(country);
+}
+
